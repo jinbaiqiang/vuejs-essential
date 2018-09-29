c969d77ba6a92224f5e614f1384dd54436c6d24d
jinbaiqiang <739148823@qq.com>
2018-09-29 17:14:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+
+<!-- 根组件 -->
+
+<!--<template>-->
+  <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+    <!--<h1>Welcome to VuejsCaff.com</h1>-->
+  <!--</div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import HelloWorld from './components/HelloWorld'-->
+
+<!--export default {-->
+  <!--name: 'App',-->
+  <!--components: {-->
+    <!--HelloWorld-->
+  <!--}-->
+<!--}-->
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+  <!--font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+  <!-- -webkit-font-smoothing: antialiased;-->
+  <!-- -moz-osx-font-smoothing: grayscale;-->
+  <!--text-align: center;-->
+  <!--color: #2c3e50;-->
+  <!--margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
+
+
+
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 200px;
+
+    h1 {
+      color: #42b983;
+    }
+  }
+</style>
